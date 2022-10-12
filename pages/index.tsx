f2187a4975b114b5dbe1cbb320e55e1a72241e6a
Linus Foxell <linusfoxell@hotmail.com>
2022-10-12 12:04:20 +0200

    add: styling about section code

--- pages/index.tsx
+++ pages/index.tsx
@@ -123,37 +123,101 @@ const Home: NextPage = () => {
       <section className="min-h-screen w-full flex flex-col justify-center items-center">
         <h1 className="text-4xl font-semibold my-32">{"<About/>"}</h1>
         <div className="w-full flex justify-center items-center">
-          <div className="flex items-center justify-center w-1/2 h-[30rem] dark:bg-gradient-to-r from-violet-500 to-fuchsia-500 rounded-3xl ml-2 ">
-            <div className="w-[95%] h-[95%] dark:bg-gray-800 rounded-3xl">
-              <code className="flex justify-start flex-col ml-4 mt-4 font-code text-gray-300">
-                <span className="mb-1">{"export const Linus: Person {"}</span>
-                <span className="">
-                  &nbsp;&nbsp;&nbsp;{"name: 'Linus Foxell',"}
+          <div className="flex items-center justify-center w-1/2 h-[30rem] bg-gradient-to-r from-violet-500 to-fuchsia-500 rounded-3xl ml-2 ">
+            <div className="w-[95%] h-[95%] bg-slate-800 rounded-2xl">
+              <code className="flex justify-start flex-col ml-4 mt-4 font-code font-semibold text-gray-300">
+                <span className="mb-1">
+                  <span className="text-purple-500">export const</span>{" "}
+                  <span className="text-blue-500">Linus</span>
+                  <span className="text-cyan-500">{": Person "}</span>
+                  <span className="text-purple-500"> = </span> {" {"}
                 </span>
-                <span className="">
-                  &nbsp;&nbsp;&nbsp;{"date: new Date('1995-10-29T03:24:00')"}
+                <span>
+                  &nbsp;&nbsp;&nbsp;{"name: "}{" "}
+                  <span className="text-green-500">{"'Linus Foxell'"}</span>
+                  {","}
                 </span>
-                <span className="">
-                  &nbsp;&nbsp;&nbsp;{"occupation: 'student'"}
+                <span>
+                  &nbsp;&nbsp;&nbsp;{"date:"}
+                  <span className="text-cyan-500"> new</span>
+                  <span className="text-blue-500"> Date</span>
+                  <span className="text-purple-500">(</span>
+                  <span className="text-green-500">
+                    {"'1995-10-29T03:24:00'"}
+                  </span>
+                  <span className="text-purple-500">)</span>
+                  {","}
                 </span>
-                <span className="">
-                  &nbsp;&nbsp;&nbsp;{"residence: 'Uppsala, Sweden'"}
+                <span>
+                  &nbsp;&nbsp;&nbsp;{"occupation: "}
+                  <span className="text-green-500">{"'student'"}</span>
+                  {","}
                 </span>
-                <span className="">
-                  &nbsp;&nbsp;&nbsp;{"techStack: linusStack,"}
+                <span>
+                  &nbsp;&nbsp;&nbsp;{"residence: "}
+                  <span className="text-green-500">{"'Uppsala, Sweden'"}</span>
+                  {","}
+                </span>
+                <span>
+                  &nbsp;&nbsp;&nbsp;{"techStack:"}
+                  <span className="text-blue-600"> linusStack</span>
+                  {","}
                 </span>
                 <span>{"}"}</span>
 
                 <span className="mb-1 mt-4">
-                  {"const linusStack: Array<tech> = ["}
+                  <span className="text-purple-500">const </span>
+                  <span className="text-blue-600">linusStack</span>
+                  <span className="text-cyan-500">: Array</span>
+                  <span className="text-yellow-500">{"<"}</span>
+                  <span className="text-blue-500">tech</span>
+                  <span className="text-yellow-500">{"> "}</span>
+                  <span className="text-purple-500">= </span>
+                  {"["}
                 </span>
-                <span className="">&nbsp;&nbsp;&nbsp;{"React,"}</span>
-                <span className="">&nbsp;&nbsp;&nbsp;{"TypeScript,"}</span>
-                <span className="">&nbsp;&nbsp;&nbsp;{"JavaScript,"}</span>
-                <span className="">&nbsp;&nbsp;&nbsp;{"TailwindCSS,"}</span>
-                <span className="">&nbsp;&nbsp;&nbsp;{"SASS,"}</span>
-                <span className="">&nbsp;&nbsp;&nbsp;{"NodeJS,"}</span>
-                <span className="">&nbsp;&nbsp;&nbsp;{"ExpressJS,"}</span>
+                <span>
+                  <span className="text-blue-500">
+                    &nbsp;&nbsp;&nbsp;{"React"}
+                  </span>
+                  {","}
+                </span>
+                <span>
+                  <span className="text-blue-500">
+                    &nbsp;&nbsp;&nbsp;{"TypeScript"}
+                  </span>
+                  {","}
+                </span>
+
+                <span>
+                  <span className="text-blue-500">
+                    &nbsp;&nbsp;&nbsp;{"JavaScript"}
+                  </span>
+                  {","}
+                </span>
+                <span>
+                  <span className="text-blue-500">
+                    &nbsp;&nbsp;&nbsp;{"TailwindCSS"}
+                  </span>
+                  {","}
+                </span>
+                <span>
+                  <span className="text-blue-500">
+                    &nbsp;&nbsp;&nbsp;{"SASS"}
+                  </span>
+                  {","}
+                </span>
+                <span>
+                  <span className="text-blue-500">
+                    &nbsp;&nbsp;&nbsp;{"NodeJS"}
+                  </span>
+                  {","}
+                </span>
+                <span>
+                  <span className="text-blue-500">
+                    &nbsp;&nbsp;&nbsp;{"ExpressJS"}
+                  </span>
+                  {","}
+                </span>
                 <span>{"]"}</span>
               </code>
             </div>
